cab47ffd8bde7b7544be1af71bee5aed9a6373b5
mowuj <mowuj92@gmail.com>
2022-05-08 16:54:32 +0600

    create login component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 
+import { ToastContainer } from 'react-bootstrap';
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Login from './components/Auth/Login/Login';
+import About from './components/Home/About/About';
+import Footer from './components/Home/Footer/Footer';
 import Header from './components/Home/Header/Header';
 import Home from './components/Home/Home/Home';
+import NotFound from './components/Shared/NotFound/NotFound';
 
 function App() {
   return (
@@ -10,9 +15,13 @@ function App() {
       <Header></Header>
       <Routes>
         <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/home' element={<Home></Home>}></Route>
-        
+        <Route path='/home' element={<Home></Home>}></Route>        
+        <Route path='about' element={<About></About>}></Route>        
+        <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
+      <Footer></Footer>
+      <ToastContainer />
     </div>
   );
 }
