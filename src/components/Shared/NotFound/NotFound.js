e471910225470c2803e64fb67642973599628233
mowuj <mowuj92@gmail.com>
2022-05-08 15:45:06 +0600

    create not found router

---
+++ src/components/Shared/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import notFound from '../../../images/notFound.jpg'
+const NotFound = () => {
+    return (
+        <div className='w-50 mx-auto'>
+            
+            <img className='w-100 ' src={notFound} alt="" />
+        </div>
+    );
+};
+
+export default NotFound;
