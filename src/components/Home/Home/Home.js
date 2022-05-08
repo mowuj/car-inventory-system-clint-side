8b7d801c6ea380ba1768367907832057e25bb35f
mowuj <mowuj92@gmail.com>
2022-05-08 20:38:51 +0600

    add banner component

--- src/components/Home/Home/Home.js
+++ src/components/Home/Home/Home.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import Banner from '../Banner/Banner';
 import Footer from '../Footer/Footer';
 
 const Home = () => {
     return (
         <div>
+            <Banner></Banner>
             <Footer></Footer>
         </div>
     );
