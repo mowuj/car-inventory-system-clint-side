674b4f03cf22583be6a15918dcdff625903fde20
mowuj <mowuj92@gmail.com>
2022-05-11 16:38:18 +0600

    change banner img

--- src/components/Home/Banner/Banner.js
+++ src/components/Home/Banner/Banner.js
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import { Carousel } from 'react-bootstrap';
-import banner1 from '../../../images/banner/banner1.jpg'
-import banner2 from '../../../images/banner/banner2.jpeg'
-import banner3 from '../../../images/banner/banner3.jpeg'
+import banner1 from '../../../images/banner/banner1.png'
+import banner2 from '../../../images/banner/banner2.jpg'
+import banner3 from '../../../images/banner/banner3.jpg'
+import './Banner.css'
 const Banner = () => {
     const [index, setIndex] = useState(0);
 
@@ -12,8 +13,8 @@ const Banner = () => {
     return (
          <Carousel activeIndex={index} onSelect={handleSelect}>
       <Carousel.Item>
-        <img height={'400px'}
-          className="d-block w-100"
+        <img 
+          className="b-img d-block w-100"
           src={banner1}
           alt="First slide"
         />
@@ -23,8 +24,8 @@ const Banner = () => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img height={'400px'}
-          className="d-block w-100"
+        <img 
+          className="b-img d-block w-100"
           src={banner2}
           alt="Second slide"
         />
@@ -35,8 +36,8 @@ const Banner = () => {
         </Carousel.Caption>
       </Carousel.Item>
       <Carousel.Item>
-        <img height={'400px'}
-          className="d-block w-100"
+        <img 
+          className="b-img d-block w-100"
           src={banner3}
           alt="Third slide"
         />
